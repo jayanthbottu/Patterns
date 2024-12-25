9246836d0f4c96d5ce4d093fc71fe48f5e394323
Jayanth Bottu <147061704+jayanthbottu@users.noreply.github.com>
2024-12-25 20:20:04 +0530

    code

---
+++ Concentric Square/main.py
@@ -0,0 +1,18 @@
+n = int(input())
+for i in range(1,2*n):
+    k = n
+    if(i<=n):
+        for j in range(1,2*n):
+            print(k,end=" ")
+            if(i>j):
+                k-=1
+            if(i+j)>=2*n:
+                k+=1
+    if(i>n):
+        for j in range(1,2*n):
+            print(k,end=" ")
+            if(i+j<2*n):
+                k-=1
+            if(j>=i):
+                k+=1
+    print()
